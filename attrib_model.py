199c24464e5d2a77bf62c1298240d638825cadf1
Kamil Monicz <kamil@monicz.pl>
2023-07-30 02:39:59 +0200

    AUC 0.970, 0.904

--- attrib_model.py
+++ attrib_model.py
@@ -3,25 +3,29 @@ from math import ceil
 from typing import Sequence
 
 import numpy as np
+import tensorflow as tf
 from keras.applications import MobileNetV3Large
 from keras.callbacks import (EarlyStopping, ModelCheckpoint, ReduceLROnPlateau,
                              TensorBoard, TerminateOnNaN)
+from keras.experimental import CosineDecay
 from keras.layers import BatchNormalization, Dense, Dropout, Flatten, Input
 from keras.losses import BinaryFocalCrossentropy
-from keras.metrics import AUC
+from keras.metrics import (AUC, F1Score, FBetaScore, PrecisionAtRecall,
+                           RecallAtPrecision)
 from keras.models import Model
-from keras.optimizers import Adam
+from keras.optimizers import AdamW
 from keras.preprocessing.image import ImageDataGenerator
-from sklearn.metrics import confusion_matrix, precision_score
+from sklearn.metrics import (confusion_matrix, precision_recall_curve,
+                             precision_score, recall_score, roc_auc_score)
 from sklearn.model_selection import train_test_split
 
 from attrib_dataset import AttribDatasetEntry, iter_attrib_dataset
 from config import (ATTRIB_CONFIDENCES, ATTRIB_MODEL_PATH,
                     ATTRIB_MODEL_RESOLUTION, ATTRIB_NUM_CLASSES, DATA_DIR,
                     SEED)
-from model_save_fix import model_save_fix
 
-_BATCH_SIZE = 24
+_BATCH_SIZE = 32
+_EPOCHS = 50
 
 
 def _split_x_y(dataset: Sequence[AttribDatasetEntry]) -> tuple[np.ndarray, np.ndarray]:
@@ -30,20 +34,26 @@ def _split_x_y(dataset: Sequence[AttribDatasetEntry]) -> tuple[np.ndarray, np.nd
     return X, y
 
 
-def get_attrib_model() -> Model:
+def get_attrib_model(imagenet_weights: bool = True) -> Model:
     image_inputs = Input(shape=(ATTRIB_MODEL_RESOLUTION, ATTRIB_MODEL_RESOLUTION, 3))
     image_model = MobileNetV3Large(include_top=False,
+                                   weights='imagenet' if imagenet_weights else None,
                                    input_tensor=image_inputs,
+                                   dropout_rate=0.3,
                                    include_preprocessing=False)
+
+    freeze_ratio = 0.7
+    for layer in image_model.layers[:int(len(image_model.layers) * freeze_ratio)]:
+        layer.trainable = False
 
     z = image_model(image_inputs)
     z = Flatten()(z)
+    z = Dropout(0.3)(z)
     z = BatchNormalization()(z)
-    z = Dropout(0.2)(z)
     z = Dense(256, activation='relu')(z)
-    z = Dropout(0.2)(z)
+    z = Dropout(0.3)(z)
     z = Dense(128, activation='relu')(z)
-    z = Dropout(0.2)(z)
+    z = Dropout(0.3)(z)
     z = Dense(64, activation='relu')(z)
     z = Dense(ATTRIB_NUM_CLASSES, activation='sigmoid')(z)
 
@@ -55,15 +65,15 @@ def get_attrib_model() -> Model:
 def create_attrib_model():
     dataset = tuple(iter_attrib_dataset())
 
-    train, temp = train_test_split(dataset,
-                                   test_size=0.3,
-                                   random_state=SEED,
-                                   stratify=tuple(map(lambda x: x.labels.encode_num(), dataset)))
+    train, holdout = train_test_split(dataset,
+                                      test_size=0.3,
+                                      random_state=SEED,
+                                      stratify=tuple(map(lambda x: x.labels.encode_num(), dataset)))
 
-    holdout, test = train_test_split(temp,
-                                     test_size=2/3,
-                                     random_state=SEED,
-                                     stratify=tuple(map(lambda x: x.labels.encode_num(), temp)))
+    _, test = train_test_split(holdout,
+                               test_size=2/3,
+                               random_state=SEED,
+                               stratify=tuple(map(lambda x: x.labels.encode_num(), holdout)))
 
     X_train, y_train = _split_x_y(train)
     X_test, y_test = _split_x_y(test)
@@ -73,32 +83,41 @@ def create_attrib_model():
     # test: 20%
     # val: 10%
 
+    steps_per_epoch = ceil(len(train) / _BATCH_SIZE)
+
     datagen = ImageDataGenerator(
-        width_shift_range=0.05,
-        height_shift_range=0.05,
+        width_shift_range=0.15,
+        height_shift_range=0.15,
         rotation_range=180,
         shear_range=20,
-        zoom_range=0.15,
+        zoom_range=0.2,
+        channel_shift_range=0.1,
         fill_mode='reflect',
         horizontal_flip=True,
         vertical_flip=True,
     )
 
+    model = get_attrib_model()
+    model.compile(
+        optimizer=AdamW(
+            CosineDecay(initial_learning_rate=1e-5,
+                        decay_steps=steps_per_epoch * _EPOCHS - 8,
+                        alpha=0.3,
+                        warmup_target=3e-5,
+                        warmup_steps=steps_per_epoch * 8,),
+            amsgrad=True),
+        loss=BinaryFocalCrossentropy(apply_class_balancing=True),
+        metrics=[
+            AUC(multi_label=True, num_labels=ATTRIB_NUM_CLASSES),
+            RecallAtPrecision(0.995, class_id=0),
+            # F1Score('weighted', threshold=0.5),
+            # FBetaScore('weighted', beta=0.5, threshold=0.5),
+        ],
+    )
+
     callbacks = [
-        ReduceLROnPlateau(factor=0.2,
-                          min_lr=0.00001,
-                          cooldown=5,
-                          patience=10,
-                          min_delta=0.0005,
-                          verbose=1),
-
-        EarlyStopping('val_auc', mode='max',
-                      min_delta=0.0005,
-                      patience=25,
-                      verbose=1),
-
         ModelCheckpoint(str(ATTRIB_MODEL_PATH), 'val_auc', mode='max',
-                        initial_value_threshold=0.9,
+                        # initial_value_threshold=0.6,
                         save_best_only=True,
                         save_weights_only=True,
                         verbose=1),
@@ -107,24 +126,13 @@ def create_attrib_model():
         TerminateOnNaN(),
     ]
 
-    model = get_attrib_model()
-
-    model_save_fix(model)
-    model.compile(
-        optimizer=Adam(learning_rate=0.0005),
-        loss=BinaryFocalCrossentropy(apply_class_balancing=True),
-        metrics=[AUC(multi_label=True, num_labels=ATTRIB_NUM_CLASSES)],
-    )
-
-    steps_per_epoch = ceil(len(train) / _BATCH_SIZE)
-
-    model.fit(
-        datagen.flow(X_train, y_train, batch_size=_BATCH_SIZE),
-        epochs=1000,
-        steps_per_epoch=steps_per_epoch,
-        validation_data=(X_test, y_test),
-        callbacks=callbacks,
-    )
+    # model.fit(
+    #     datagen.flow(X_train, y_train, batch_size=_BATCH_SIZE),
+    #     epochs=_EPOCHS + 10,
+    #     steps_per_epoch=steps_per_epoch,
+    #     validation_data=(X_test, y_test),
+    #     callbacks=callbacks,
+    # )
 
     model.load_weights(str(ATTRIB_MODEL_PATH))
 
@@ -140,6 +148,9 @@ def create_attrib_model():
         y_pred_proba_class = y_pred_proba[:, i]
         y_pred_class = y_pred_proba[:, i] > threshold
 
+        auc = roc_auc_score(y_true_class, y_pred_proba_class)
+        print(f'AUC: {auc:.3f}')
+
         precision = precision_score(y_true_class, y_pred_class)
         print(f'Precision: {precision:.3f}')
 
@@ -148,6 +159,8 @@ def create_attrib_model():
         print(f'[❗] False Positives: {fp}')
         print(f'False Negatives: {fn}')
         print(f'[✅] True Positives: {tp}')
+        print()
+        print(f'Recall: {recall_score(y_true_class, y_pred_class):.3f}')
         print()
 
         for pred, proba, true, entry in sorted(zip(y_pred_class, y_pred_proba_class, y_true_class, holdout), key=lambda x: x[3].id.lower()):
