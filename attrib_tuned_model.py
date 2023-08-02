1a4ac942a8f4368a413974f80bd0a9db239d6d1f
Kamil Monicz <kamil@monicz.pl>
2023-08-03 01:46:53 +0200

    Adjust AttribTunedModel

--- attrib_tuned_model.py
+++ attrib_tuned_model.py
@@ -3,7 +3,7 @@ from typing import NamedTuple, Sequence
 import numpy as np
 import tensorflow as tf
 
-from config import ATTRIB_CONFIDENCES, ATTRIB_MODEL_PATH
+from config import ATTRIB_CONFIDENCE, ATTRIB_MODEL_PATH
 from crossing_type import CrossingType
 
 
@@ -18,21 +18,20 @@ class AttribTunedModel:
         self._model = get_attrib_model()
         self._model.load_weights(str(ATTRIB_MODEL_PATH))
 
-    def predict_single(self, X: np.ndarray, min_confidences: Sequence[float] = ATTRIB_CONFIDENCES) -> AttribClassification:
+    def predict_single(self, X: np.ndarray, min_confidence: float = ATTRIB_CONFIDENCE) -> AttribClassification:
         with tf.device('/CPU:0'):  # force CPU to better understand real performance
             pred_proba: dict = self._model.predict(X[np.newaxis, ...], verbose=0)[0]
 
-        assert len(pred_proba) == len(min_confidences)
+        assert len(pred_proba) == 1
 
-        is_valid = pred_proba[0] > min_confidences[0]
-        is_uncontrolled = pred_proba[1] < min_confidences[1][0]
-        is_traffic_signals = pred_proba[1] > min_confidences[1][1]
+        is_valid = pred_proba[0] > min_confidence
+        crossing_type = CrossingType.UNKNOWN
 
-        if (not is_uncontrolled and not is_traffic_signals) or (is_uncontrolled and is_traffic_signals):
-            crossing_type = CrossingType.UNKNOWN
-        elif is_uncontrolled:
-            crossing_type = CrossingType.UNCONTROLLED
-        elif is_traffic_signals:
-            crossing_type = CrossingType.TRAFFIC_SIGNALS
+        # if (not is_uncontrolled and not is_traffic_signals) or (is_uncontrolled and is_traffic_signals):
+        #     crossing_type = CrossingType.UNKNOWN
+        # elif is_uncontrolled:
+        #     crossing_type = CrossingType.UNCONTROLLED
+        # elif is_traffic_signals:
+        #     crossing_type = CrossingType.TRAFFIC_SIGNALS
 
         return AttribClassification(is_valid, crossing_type)
