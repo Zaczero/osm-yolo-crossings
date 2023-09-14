d3390cd7e94b9b278bcb4702e75b663a75155fc1
Kamil Monicz <kamil@monicz.pl>
2023-09-14 13:03:08 +0200

    Fix cuda load

--- shell.nix
+++ shell.nix
@@ -22,7 +22,7 @@ with pkgs; let
   '';
 
   devShellHook = ''
-    export LD_LIBRARY_PATH="${lib.makeLibraryPath commonBuildInputs}"
+    export LD_LIBRARY_PATH="${lib.makeLibraryPath commonBuildInputs}:$LD_LIBRARY_PATH"
     export PIPENV_VENV_IN_PROJECT=1
     export PIPENV_VERBOSITY=-1
     [ ! -f .venv/bin/activate ] && pipenv sync --dev
